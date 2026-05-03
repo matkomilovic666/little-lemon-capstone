54ca08618af21abaf7a8b4d6a12a2c0e58553bf2
Matko Milovic <37634969+matkomilovic666@users.noreply.github.com>
2026-05-03 18:00:43 -0400

    Basic setup for the HTML docs

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+function Footer() {
+    return (
+        <footer className="footer-section">
+            <div className="footer-column">
+                <h3>Contact Us</h3>
+                <p>Phone: +24500-03</p>
+                <p>Email: contactus@littlelemon.com</p>
+                <p>Address: 12345 North Star, Santiago, Chile</p>
+            </div>
+            <div className="footer-column">
+                <ul className="social-media-links">
+                    <li>
+                        <a href="#">
+                            Facebook
+                            <FontAwesomeIcon icon={faFacebookF} className="icon" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Twitter
+                            <FontAwesomeIcon icon={faTwitter} className="icon" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Instagram
+                            <FontAwesomeIcon icon={faInstagram} className="icon" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
